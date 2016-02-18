2a2314e59ea65649d97d208fa965993338d7cf42
Adri <adry21ciru@gmail.com>
2016-02-18 20:06:03 +0100

    Solution - Backend developer

--- backend.py
+++ backend.py
@@ -24,6 +24,9 @@ class BackendDeveloper(object):
                 Output should be 0 for candidates that do not fulfill any requirements
 
         """
+        lKnowledges = self.knowledges
+        lRequirements = self.key_requirements + self.other_requirements
+        return len(set(lKnowledges) & set(lRequirements))
 
     def i_am_ready(self):
         if self.score > 7:
@@ -32,6 +35,8 @@ class BackendDeveloper(object):
             print ("Wait, you have created successfully the score method? Then make a pull request, we want you!")
 
 if __name__ == "__main__":
-    """
-        Write the code to create yourself, a BackendDeveloper, and let us know you are ready!
-    """
+	"""
+		Write the code to create yourself, a BackendDeveloper, and let us know you are ready!
+	"""
+	BD = BackendDeveloper("Adrián","Sáez Clemente","adry21ciru@gmail.com",('python','django','motivation','software','git','teamwork','linux','REST','star wars'))
+	BD.i_am_ready()
